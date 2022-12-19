3e9ea6af952a5f0bfcd79c6f36b7a0a3d556f517
nttnguyen136 <nttnguyen136@gmail.com>
2022-12-19 17:44:38 +0700

    update code 13

--- contracts/cw20-base/src/contract.rs
+++ contracts/cw20-base/src/contract.rs
@@ -148,7 +148,7 @@ pub fn instantiate(
         MARKETING_INFO.save(deps.storage, &data)?;
     }
 
-    Ok(Response::new().add_attribute("instantiate", "12"))
+    Ok(Response::new().add_attribute("instantiate", "13"))
 }
 
 pub fn create_accounts(deps: &mut DepsMut, accounts: &[Cw20Coin]) -> StdResult<Uint128> {
