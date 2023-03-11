85d40db1eeb73f761c336a8866f455894fca6aac
nttnguyen136 <nttnguyen136@gmail.com>
2023-03-11 11:27:56 +0700

    update code 14

--- contracts/cw20-base/src/contract.rs
+++ contracts/cw20-base/src/contract.rs
@@ -267,7 +267,7 @@ pub fn execute_transfer(
         .add_attribute("from", info.sender)
         .add_attribute("to", recipient)
         .add_attribute("amount", amount)
-        .add_attribute("BuildCode", '13');
+        .add_attribute("BuildCode", '14');
     Ok(res)
 }
 
